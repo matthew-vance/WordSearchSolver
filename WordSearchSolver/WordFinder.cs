95d28363786acc1c0a302a03a2b9179a75569b73
Matthew Vance <mavance44@gmail.com>
2019-02-02 16:52:40 -0500

    data drive WordFinderTests and refactor TryFindWord to pass all test cases

--- WordSearchSolver/WordFinder.cs
+++ WordSearchSolver/WordFinder.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("WordSearchSolverTests")]
 namespace WordSearchSolver
 {
-    public class WordFinder
+    internal class WordFinder
     {
         private readonly char[,] _puzzle;
 
@@ -11,8 +13,49 @@ namespace WordSearchSolver
             _puzzle = puzzle;
         }
 
-        public bool TryFindWord(string word)
+        public bool TryFindWord(string word, out int[,] location)
         {
+            var wordFound = false;
+            location = new int[word.Length, 2];
+            var wordFirstLetter = word[0];
+
+            for (int row = 0; row < _puzzle.GetLength(0); row++)
+            {
+                for (int column = 0; column < _puzzle.GetLength(1); column++)
+                {
+                    var currentPuzzleLetter = _puzzle[row, column];
+                    if (currentPuzzleLetter != wordFirstLetter)
+                        continue;
+
+                    wordFound = TrySearchDown(word, row, column, out location);
+                    if (wordFound)
+                        break;
+                }
+                if (wordFound)
+                    break;
+            }
+            return wordFound;
+        }
+
+        private bool TrySearchDown(string word, int row, int column, out int[,] location)
+        {
+            location = new int[word.Length, 2];
+
+            if (word.Length + row > _puzzle.GetLength(0))
+                return false;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                var currentRow = row + i;
+                if (_puzzle[currentRow, column] == word[i])
+                {
+                    location[i, 0] = column;
+                    location[i, 1] = currentRow;
+                }
+                else
+                    return false;
+            }
+
             return true;
         }
     }
--- WordSearchSolverTests/WordFinderTests.cs
+++ WordSearchSolverTests/WordFinderTests.cs
@@ -2,22 +2,67 @@ using System;
 using WordSearchSolver;
 using Xunit;
 using Moq;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace WordSearchSolverTests
 {
     public class WordFinderTests
     {
-        [Fact]
-        public void Should_TryFindWord_When_PassedWordInPuzzle()
-        {
-            // Arrange
-            var wordFinder = new WordFinder(It.IsAny<char[,]>());
+        WordFinder wordFinder = new WordFinder(GetMockPuzzle());
 
+        [Theory]
+        [InlineData("BONES", true)]
+        [InlineData("CHEKOV", false)]
+        public void Should_TryFindWordWithExpected_When_PassedWord(string word, bool expected)
+        {
             // Act
-            var wordFound = wordFinder.TryFindWord(It.IsAny<string>());
+            var wordFound = wordFinder.TryFindWord(word, out int[,] location);
 
             // Assert
-            Assert.True(wordFound);
+            Assert.Equal(expected, wordFound);
         }
+
+        [Theory]
+        [ClassData(typeof(ReturnLocationTestData))]
+        public void Should_ReturnWordLocation_When_PassedWordInPuzzle(string word, int[,] expected)
+        {
+            // Act
+            wordFinder.TryFindWord(word, out int[,] location);
+
+            // Assert
+            Assert.Equal(expected, location);
+        }
+
+        private static char[,] GetMockPuzzle()
+        {
+            return new char[,] { {'U','M','K','H','U','L','K','I','N','V','J','O','C','W','E'},
+                                 {'L','L','S','H','K','Z','Z','W','Z','C','G','J','U','Y','G'},
+                                 {'H','S','U','P','J','P','R','J','D','H','S','B','X','T','G'},
+                                 {'B','R','J','S','O','E','Q','E','T','I','K','K','G','L','E'},
+                                 {'A','Y','O','A','G','C','I','R','D','Q','H','R','T','C','D'},
+                                 {'S','C','O','T','T','Y','K','Z','R','E','P','P','X','P','F'},
+                                 {'B','L','Q','S','L','N','E','E','E','V','U','L','F','M','Z'},
+                                 {'O','K','R','I','K','A','M','M','R','M','F','B','A','P','P'},
+                                 {'N','U','I','I','Y','H','Q','M','E','M','Q','R','Y','F','S'},
+                                 {'E','Y','Z','Y','G','K','Q','J','P','C','Q','W','Y','A','K'},
+                                 {'S','J','F','Z','M','Q','I','B','D','B','E','M','K','W','D'},
+                                 {'T','G','L','B','H','C','B','E','C','H','T','O','Y','I','K'},
+                                 {'O','J','Y','E','U','L','N','C','C','L','Y','B','Z','U','H'},
+                                 {'W','Z','M','I','S','U','K','U','R','B','I','D','U','X','S'},
+                                 {'K','Y','L','B','Q','Q','P','M','D','F','C','K','E','A','B'}
+            };
+        }
+    }
+
+    internal class ReturnLocationTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { "BONES", new int[5, 2] { { 0, 6 }, { 0, 7 }, { 0, 8 }, { 0, 9 }, { 0, 10 } }};
+            yield return new object[] { "CHEKOV", new int[6,2]};
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
