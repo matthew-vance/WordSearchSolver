bc5a35e778a3d6eba7587ba13302b7145995988c
Matthew Vance <mavance44@gmail.com>
2019-02-04 17:22:14 -0500

    create Coordinate class

---
+++ WordSearchSolver/Coordinate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearchSolver
+{
+    internal class Coordinate
+    {
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+
+        public Coordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
---
+++ WordSearchSolverTests/CoordinateTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WordSearchSolver;
+using Xunit;
+
+namespace WordSearchSolverTests
+{
+    public class CoordinateTests
+    {
+        [Fact]
+        public void Should_CreateCoordinateWithRowAndColumn_When_InstantiatedWithRowAndColumn()
+        {
+            // Arrange
+            var row = It.IsAny<int>();
+            var column = It.IsAny<int>();
+
+            // Act
+            var coordinate = new Coordinate(row, column);
+
+            // Assert
+            Assert.Equal(row, coordinate.Row);
+            Assert.Equal(column, coordinate.Column);
+        }
+    }
+}
