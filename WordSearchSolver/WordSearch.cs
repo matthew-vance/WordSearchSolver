f2fba778382ce7783bd613f56f9aac2be9bd9224
Matthew Vance <mavance44@gmail.com>
2019-02-04 15:45:47 -0500

    create WordSearch class with constructor

---
+++ WordSearchSolver/WordSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearchSolver
+{
+    public class WordSearch
+    {
+        public readonly IList<string> Words;
+        public readonly char[,] Puzzle;
+
+        public WordSearch(IList<string> words, char[,] puzzle)
+        {
+            Words = words;
+            Puzzle = puzzle;
+        }
+    }
+}
---
+++ WordSearchSolverTests/WordSearchTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using WordSearchSolver;
+using System.Collections.Generic;
+using Xunit;
+
+namespace WordSearchSolverTests
+{
+    public class WordSearchTests
+    {
+        [Fact]
+        public void Should_CreateWordSearchWIthWordsAndPuzzle_When_InstantiatedWithWordsAndPuzzle()
+        {
+            // Arrange
+            var words = new List<string>() { "BONES", "CHEKOV", "KHAN", "KIRK", "SCOTTY", "SPOCK", "SULU", "UHURA", "COMPUTER" };
+            var puzzle = GetMockPuzzle();
+
+            // Act
+            var wordSearch = new WordSearch(words, puzzle);
+
+            // Assert
+            Assert.Equal(words, wordSearch.Words);
+            Assert.Equal(puzzle, wordSearch.Puzzle);
+        }
+
+        private static char[,] GetMockPuzzle()
+        {
+            return new char[,] { {'U','M','K','H','U','L','K','I','N','V','J','O','C','W','E'},
+                                 {'L','L','S','H','K','Z','Z','W','Z','C','G','J','U','Y','G'},
+                                 {'H','S','U','P','J','P','R','J','D','H','S','B','X','T','G'},
+                                 {'B','R','J','S','O','E','Q','E','T','I','K','K','G','L','E'},
+                                 {'A','Y','O','A','G','C','I','R','D','Q','H','R','T','C','D'},
+                                 {'S','C','O','T','T','Y','K','Z','R','E','P','P','X','P','R'},
+                                 {'B','L','Q','S','L','N','E','E','E','V','U','L','F','E','Z'},
+                                 {'O','K','R','I','K','A','M','M','R','M','F','B','T','P','P'},
+                                 {'N','U','I','I','Y','H','Q','M','E','M','Q','U','Y','F','S'},
+                                 {'E','Y','Z','Y','G','K','Q','J','P','C','P','W','Y','A','K'},
+                                 {'S','J','F','Z','M','Q','I','B','D','M','E','M','K','W','D'},
+                                 {'T','G','L','B','H','C','B','E','O','H','T','O','Y','I','K'},
+                                 {'O','J','Y','E','U','L','N','C','C','L','Y','B','Z','U','H'},
+                                 {'W','Z','M','I','S','U','K','U','R','B','I','D','U','X','S'},
+                                 {'K','Y','L','B','Q','Q','P','M','D','F','C','K','E','A','B'}
+            };
+        }
+    }
+}
